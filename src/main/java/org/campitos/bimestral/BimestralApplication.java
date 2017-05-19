94fc466ca1d7d2b558fe7c288d48f7b62e00f6a2
daton <rapidclimate@gmail.com>
2017-05-19 13:01:40 -0500

    Viernes 18 may 2017

---
+++ src/main/java/org/campitos/bimestral/BimestralApplication.java
@@ -0,0 +1,12 @@
+package org.campitos.bimestral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BimestralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BimestralApplication.class, args);
+	}
+}
